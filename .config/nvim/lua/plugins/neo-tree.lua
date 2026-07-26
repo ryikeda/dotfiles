f58cde9a81ca13a4bca23cd9e1b2185577aaeee3
Rafael Ikeda <ryikeda@outlook.com>
2026-07-26 14:43:37 +0900

    fix: make dotfiles visible bt default

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -6,7 +6,9 @@ return {
     },
     filesystem = {
       filtered_items = {
+        visible = true,
         hide_dotfiles = false,
+        hide_gitignored = false,
         hide_by_name = {
           ".git",
           ".DS_Store",
